d9b00a667c9067bee09dd9443991cfe43765ebe2
andrew-lod <90393250+andrew-lod@users.noreply.github.com>
2024-10-21 22:22:41 +0200

    lazygit

--- init.lua
+++ init.lua
@@ -947,6 +947,7 @@ require('lazy').setup({
   require 'plugins.chatGPT',
   require 'plugins.command-completion',
   require 'plugins.kulala',
+  require 'plugins.lazygit',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
